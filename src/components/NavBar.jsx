74e97d1652903c3887261b0629a48603bc57b3f3
saikrishna <potharaboinasaikrishna1@gmail.com>
2026-09-10 08:33:24 +0530

    added Link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 
 export function NavBar() {
@@ -9,7 +10,7 @@ export function NavBar() {
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">DevTinder</a>
+                <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
             </div>
             {user && <div className="flex gap-2">
                 <div className="flex items-center">
@@ -27,10 +28,10 @@ export function NavBar() {
                         tabIndex={-1}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <li>
-                            <a className="justify-between">
+                            <Link to="/profile" className="justify-between">
                                 Profile
                                 <span className="badge">New</span>
-                            </a>
+                            </Link>
                         </li>
                         <li><a>Settings</a></li>
                         <li><a>Logout</a></li>
