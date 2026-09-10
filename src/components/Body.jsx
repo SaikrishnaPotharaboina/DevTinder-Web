661f62baaef7306b38e654095ef921b29b069c24
saikrishna <potharaboinasaikrishna1@gmail.com>
2026-09-10 08:33:13 +0530

    created /profile/view api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { NavBar } from "./NavBar";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { addUser } from "../utils/userSlice";
 
 
 const Body = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store) => store.user?.data);
+
+    const fetchUser = async () => {
+        try {
+            const res = await axios.get(
+                BASE_URL + "/profile/view",
+                { withCredentials: true }
+            );
+
+            dispatch(addUser(res?.data));
+        } catch (error) {
+            if (error.response?.status === 401) {
+                navigate("/login");
+            }
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        if (!userData) {
+            fetchUser();
+        }
+    }, []);
     return (
         <div>
             <NavBar />
