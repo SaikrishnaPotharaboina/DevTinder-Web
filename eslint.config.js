a99ac2396fab0328e60199913699b36dff12f296
saikrishna <potharaboinasaikrishna1@gmail.com>
2026-09-12 18:49:35 +0530

    changed rules

--- eslint.config.js
+++ eslint.config.js
@@ -17,14 +17,5 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
   },
 ])
