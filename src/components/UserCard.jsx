429d8220be8743daa822b4e4bf67278e7566be62
saikrishna <potharaboinasaikrishna1@gmail.com>
2026-09-13 16:17:30 +0530

    userCard functional

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,29 +1,44 @@
-
-
 function UserCard({ user }) {
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
     const { firstName, lastName, age, gender, photoUrl, about } = user;
+
     return (
-        <div className="my-20  flex justify items-center" >
-            <div className="card bg-base-300 w-85 shadow-sm ">
+        <div className="my-15 flex justify-center items-center">
+            <div className="card bg-base-300 w-85 shadow-sm">
                 <figure>
                     <img
                         src={photoUrl}
                         alt="photo"
-
                     />
                 </figure>
+
                 <div className="card-body">
-                    <h2 className="card-title">{firstName + " " + lastName}</h2>
-                    {age && gender && <p>{age + ", " + gender}</p>}
+                    <h2 className="card-title">
+                        {firstName} {lastName}
+                    </h2>
+
+                    {age && gender && (
+                        <p>{age}, {gender}</p>
+                    )}
+
                     <p>{about}</p>
+
                     <div className="card-actions justify-center">
-                        <button className="btn btn-primary">Ignore</button>
-                        <button className="btn btn-secondary">Intersted</button>
+                        <button className="btn btn-primary">
+                            Ignore
+                        </button>
+
+                        <button className="btn btn-secondary">
+                            Interested
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default UserCard;
