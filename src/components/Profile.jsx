446263fbbabed8f2f11e277ced25553b6dc62d96
saikrishna <potharaboinasaikrishna1@gmail.com>
2026-09-13 16:16:59 +0530

    added store and editProfile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,15 @@
+import { useSelector } from "react-redux";
+import EditProfile from "./EditProfile";
 
 
 function Profile() {
+    const user = useSelector((store) => store.user?.data);
     return (
-        <div>Profile</div>
+        user && (
+            <div>
+                <EditProfile user={user} />
+            </div>
+        )
     )
 }
 
